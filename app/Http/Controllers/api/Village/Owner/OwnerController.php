77ff075a0b86ed93b08b092918ed2e1ef59b1413
Ahmed <Wegostationdev@gmail.com>
2025-05-08 11:38:58 +0200

    commit

--- app/Http/Controllers/api/Village/Owner/OwnerController.php
+++ app/Http/Controllers/api/Village/Owner/OwnerController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\trait\image;
+use App\Http\Requests\Village\OwnerRequest;
 
 use App\Models\User;
 
