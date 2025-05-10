bfa85394ae054f16d727636ac9223f8f4fd8e73d
Ahmed <Wegostationdev@gmail.com>
2025-05-10 11:37:39 +0200

    commit

--- routes/village.php
+++ routes/village.php
@@ -4,12 +4,22 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\api\Village\Owner\OwnerController;
+use App\Http\Controllers\api\Village\Pools\PoolController;
 
 Route::middleware(['auth:sanctum', 'IsVillage'])->group(function(){
     Route::controller(OwnerController::class)->prefix('owner')
     ->group(function() {
         Route::get('/', 'view');
         Route::get('/item/{id}', 'owner');
+        Route::put('/status/{id}', 'status');
+        Route::post('/add', 'create');
+        Route::post('/update/{id}', 'modify');
+        Route::delete('/delete/{id}', 'delete');
+    });
+
+    Route::controller(PoolController::class)->prefix('pool')
+    ->group(function() {
+        Route::get('/', 'view');
         Route::put('/status/{id}', 'status');
         Route::post('/add', 'create');
         Route::post('/update/{id}', 'modify');
