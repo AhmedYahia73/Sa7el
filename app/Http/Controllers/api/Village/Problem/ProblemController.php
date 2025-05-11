2f060e00cf21c8191ad2c539824bf0408ed21fe4
Ahmed <Wegostationdev@gmail.com>
2025-05-11 11:17:12 +0200

    commit

--- app/Http/Controllers/api/Village/Problem/ProblemController.php
+++ app/Http/Controllers/api/Village/Problem/ProblemController.php
@@ -21,12 +21,6 @@ class ProblemController extends Controller
         ->get()
         ->map(function($item) use($appartment, $request){
             $appartment = $appartment
-            ->where('village_id', $request->user()->village_id)
-            ->where('user_id', $request->user()->id)
-            ->where('type', 'owner')
-            ->orWhere('village_id', $request->user()->village_id)
-            ->where('user_id', $request->user()->id)
-            ->orderByDesc('id')
             ->first();
             return [
                 'id' =>$item->id,
