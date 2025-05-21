792351c3d7d605a344ad63bf4688b5802cc9ba5f
Ahmed <Wegostationdev@gmail.com>
2025-05-21 14:26:17 +0200

    commit

--- app/Http/Controllers/api/Village/ServiceType/ServiceTypeController.php
+++ app/Http/Controllers/api/Village/ServiceType/ServiceTypeController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
-use App\Models\Service Type;
+use App\Models\ServiceType;
 
 class ServiceTypeController extends Controller
 {
