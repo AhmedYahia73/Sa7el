bc29cd3c50aab97b54c947cc993ba2e61a254384
Ahmed <Wegostationdev@gmail.com>
2025-05-24 11:03:41 +0200

    commit

--- app/Http/Controllers/api/Village/Visitor/VisitorController.php
+++ app/Http/Controllers/api/Village/Visitor/VisitorController.php
@@ -14,8 +14,20 @@ class VisitorController extends Controller
     public function view(Request $request){
         $visit_requests = $this->visit_request
         ->where('village_id', $request->user()->village_id)
-        ->with(['owner:id,name', 'appartment:id,unit,number_floors'])
-        ->get();
+        ->with(['owner', 'appartment'])
+        ->get()
+        ->map(function($item){
+            return [
+                'id' => $item->id,
+                'unit' => $item?->appartment?->unit,
+                'unit_type' => $item?->appartment?->unit?->type?->name,
+                'user_name' => $item?->owner?->name,
+                'user_phone' => $item?->owner?->phone,
+                'visit_type' => $item->visitor_type,
+                'date' => $item->date,
+                'time' => $item->time,
+            ];
+        });
 
         return response()->json([
             'visit_requests' => $visit_requests
