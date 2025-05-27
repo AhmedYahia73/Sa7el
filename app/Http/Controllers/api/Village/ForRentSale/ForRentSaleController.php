7bc22184bf799c7372c3f02ce06322335100313d
Ahmed <Wegostationdev@gmail.com>
2025-05-27 14:31:09 +0200

    commit

--- app/Http/Controllers/api/Village/ForRentSale/ForRentSaleController.php
+++ app/Http/Controllers/api/Village/ForRentSale/ForRentSaleController.php
@@ -13,8 +13,38 @@ class ForRentSaleController extends Controller
 
     public function view(Request $request){
         $offers = $this->offers
+        ->whereHas('offer_status', function($query){
+            $query->where('rent_status', 1)
+            ->orWhere('sale_status', 1);
+        })
         ->with('owner', 'appartment')
-        ->get();
+        ->get()
+        ->map(function($item){
+            $type_offer = null;
+            if ($item->offer_status->sale_status && $item->offer_status->rent_status) {
+                $type_offer = 'Sale & Rent';
+            }
+            elseif ($item->offer_status->sale_status) {
+                $type_offer = 'Sale';
+            }
+            elseif ($item->offer_status->rent_status) {
+                $type_offer = 'Rent';
+            }
+            return [
+                'id' => $item->id,
+                'village' => $item?->village?->name,
+                'image' => $item?->village?->image_link,
+                'cover_image' => $item?->village?->cover_image_link,
+                'owner' => $item?->owner?->name,
+                'unit' => $item?->appartment?->unit,
+                'unit' => $item?->appartment?->unit,
+                'description' => $item->description,
+                'type_offer' => $type_offer,
+                'price_day' => $item->price_day,
+                'price_month' => $item->price_month,
+                'price' => $item->price,
+            ];
+        });
 
         return response()->json([
             'offers' => $offers,
