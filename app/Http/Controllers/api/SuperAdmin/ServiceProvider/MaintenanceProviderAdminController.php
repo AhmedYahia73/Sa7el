dd5e7ee26bf285cae1a9f6854cc8222c919f9d1b
Ahmed <Wegostationdev@gmail.com>
2025-06-04 14:36:07 +0200

    commit

--- app/Http/Controllers/api/SuperAdmin/ServiceProvider/MaintenanceProviderAdminController.php
+++ app/Http/Controllers/api/SuperAdmin/ServiceProvider/MaintenanceProviderAdminController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\api\SuperAdmin\Provider;
+namespace App\Http\Controllers\api\SuperAdmin\ServiceProvider;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
