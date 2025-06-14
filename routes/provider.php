80d6e6bf820031a8762b22da78cb77f3a7dac35f
Ahmed <Wegostationdev@gmail.com>
2025-06-14 10:32:59 +0200

    commit

--- routes/provider.php
+++ routes/provider.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\api\ServiceProvider\Contact\ContactController;
+use App\Http\Controllers\api\ServiceProvider\Gallery\GalleryController;
 
 Route::middleware(['auth:sanctum', 'IsProvider'])->group(function(){
     Route::controller(ContactController::class)->prefix('contact')
