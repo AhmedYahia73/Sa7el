5b559398ab263a77c537858365b82c3e5f700bb8
Ahmed <Wegostationdev@gmail.com>
2025-06-14 10:52:28 +0200

    commit

--- routes/provider.php
+++ routes/provider.php
@@ -4,13 +4,21 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\api\ServiceProvider\Contact\ContactController;
-use App\Http\Controllers\api\ServiceProvider\Gallery\GalleryController;
+use App\Http\Controllers\api\ServiceProvider\Menue\MenueController;
 
 Route::middleware(['auth:sanctum', 'IsProvider'])->group(function(){
     Route::controller(ContactController::class)->prefix('contact')
     ->group(function() {
         Route::get('/', 'view'); 
         Route::post('/update', 'update');
+    });
+    
+    Route::controller(MenueController::class)->prefix('menue')
+    ->group(function() {
+        Route::get('/', 'view'); 
+        Route::put('/status/{id}', 'status');
+        Route::post('/add', 'create');
+        Route::delete('/delete/{id}', 'delete');
     });
     
     Route::controller(GalleryController::class)->prefix('gallery')
