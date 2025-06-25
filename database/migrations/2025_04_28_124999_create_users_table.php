7a8c0d5419377df1006dc919c5bdac2b793c820d
Ahmed <Wegostationdev@gmail.com>
2025-06-25 12:48:28 +0200

    commit

--- database/migrations/2025_04_28_124999_create_users_table.php
+++ database/migrations/2025_04_28_124999_create_users_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('phone')->nullable()->unique();
+            $table->string('phone')->nullable();
             $table->string('password');
             $table->foreignId('provider_id')->nullable()->constrained('providers')->onUpdate('cascade')->onDelete('cascade');
             $table->foreignId('village_id')->nullable()->constrained('villages')->onUpdate('cascade')->onDelete('cascade');
