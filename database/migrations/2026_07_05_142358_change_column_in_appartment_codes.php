519341b30f8bb53434d7b74bf4210efa29174d02
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-07-05 14:33:08 +0300

    dsfsd

--- database/migrations/2026_07_05_142358_change_column_in_appartment_codes.php
+++ database/migrations/2026_07_05_142358_change_column_in_appartment_codes.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('appartment_codes', function (Blueprint $table) {
-            $table->dateTime('from')nullabl()->change();
-            $table->dateTime('to')nullabl()->change();
+            $table->dateTime('from')->nullable()->change();
+            $table->dateTime('to')->nullable()->change();
         });
     }
 
