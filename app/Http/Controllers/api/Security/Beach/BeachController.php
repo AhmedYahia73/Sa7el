c653e8b82f32cc9d8c5f9c2129e701ecf790bf8b
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-07-22 09:43:32 +0300

    asds

--- app/Http/Controllers/api/Security/Beach/BeachController.php
+++ app/Http/Controllers/api/Security/Beach/BeachController.php
@@ -80,10 +80,7 @@ class BeachController extends Controller
          ->whereDate('created_at', date('Y-m-d'))
          ->sum("umbrella") ?? 0;
         $my_umbrellas = $umberllas - $user_umbrellas;
-        return response()->json([
-            'success' => 'Qr code is true',
-            "umbrellas" => $my_umbrellas,
-        ]);
+       
         if($my_umbrellas < $request->umbrella){
             return response()->json([
                 'errors' => 'عدد الشمسيات المتاحة ' . $my_umbrellas
