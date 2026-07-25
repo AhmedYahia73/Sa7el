97d7ab83c101c4995ba9476a5ffaa91d4982722f
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-07-25 16:52:48 +0300

    fsdfs

--- app/Http/Controllers/api/Security/Pool/PoolController.php
+++ app/Http/Controllers/api/Security/Pool/PoolController.php
@@ -56,6 +56,7 @@ class PoolController extends Controller
         $appartment_id = 0;
         $creates_at = null;
         $is__visitor = $arr_text[0] == 'visitor_id';
+        return $is__visitor;
         if ($arr_text[0] == 'visitor_id') {
             $userid = intval($arr_text[1]);
             $visitor_type = $arr_text[5];
