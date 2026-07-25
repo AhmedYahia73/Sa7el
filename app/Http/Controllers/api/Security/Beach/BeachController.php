49a41deba4bcdab1ed0b9da413cccb84dcf646f3
AhmedYahia73 <ahmedahmadahmid73@gmail.com>
2026-07-25 16:46:54 +0300

    fdgf

--- app/Http/Controllers/api/Security/Beach/BeachController.php
+++ app/Http/Controllers/api/Security/Beach/BeachController.php
@@ -49,8 +49,8 @@ class BeachController extends Controller
         // $qrcode = new QrReader($tempImagePath);
         // $text = $qrcode->text();
         $text = $request->qr_code;
-        $arr_text = explode('-', $text);
-        $arr_text = count($arr_text) > 1 ? $arr_text : explode('>', $text);
+        $arr_text = explode('>', $text);
+        $arr_text = count($arr_text) > 1 ? $arr_text : explode('-', $text);
         $userid = 0;
         $code = null;
         $beach_id = 0;
